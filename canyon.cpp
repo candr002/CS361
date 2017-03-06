b9955fd6874cacb89f4413a786c1b254e082b47d
candr002 <candr002@odu.edu>
2017-03-06 10:02:01 -0500

    Add files via upload
    for class

--- canyon.cpp
+++ canyon.cpp
@@ -1,18 +1,23 @@
 #include "classDefs.h"
 #include <list>
-
 bool checker(list<surface>::iterator itr);
-double numberGen(list<surface>::iterator itr);
+
 
 
 int main()
-{
+{   default_random_engine coin(time(NULL));
+    uniform_int_distribution<int> flipa(0,1);
+    uniform_int_distribution<int> tossa(1,3);
     bool checkVal;
+
+    int coinVal, d3val;
     double tempVal;
     list<surface> plane;
     list<surface>::iterator planeItr = plane.begin();
-    list<surface>::iterator planeStart = plane.begin();
-    point tempPoint1, tempPoint2, tempPoint3, tempPoint4, tempPoint5;
+    list<surface>::iterator planeDelete = plane.begin();
+    list<surface>::iterator ptr1, ptr2, ptr3;
+
+    point tempPoint1, tempPoint2, tempPoint3, tempPoint4, tempPoint5, tempPoint6;
     ofstream outfile;
     ofstream outdat;
     outfile.open ("command.txt");
@@ -22,53 +27,156 @@ int main()
     tempPoint1.setY(0);
     tempPoint1.setZ(0);
 
-    tempPoint2.setX(100);
+    tempPoint2.setX(1000);
     tempPoint2.setY(0);
     tempPoint2.setZ(0);
 
-    tempPoint3.setX(100);
-    tempPoint3.setY(100);
-    tempPoint3.setZ(0);
+    tempPoint3.setX(1000);
+    tempPoint3.setY(0);
+    tempPoint3.setZ(2000);
 
-    tempPoint4.setX(0);
-    tempPoint4.setY(100);
-    tempPoint4.setZ(0);
+    tempPoint4.setX(2000);
+    tempPoint4.setY(0);
+    tempPoint4.setZ(2000);
 
-    tempPoint5.setX(50);
-    tempPoint5.setY(50);
-    tempPoint5.setZ(200);
+    tempPoint5.setX(0);
+    tempPoint5.setY(0);
+    tempPoint5.setZ(2000);
+
+    tempPoint6.setX(3000);
+    tempPoint6.setY(0);
+    tempPoint6.setZ(2000);
 
     plane.push_front(surface(tempPoint1,tempPoint2,tempPoint5));
-    planeItr = plane.begin();
+    ptr1 = plane.begin();
+
+    plane.push_front(surface(tempPoint5,tempPoint2,tempPoint3));
+    ptr2 = plane.begin();
+
+    plane.push_front(surface(tempPoint2,tempPoint4,tempPoint3));
+    ptr3 = plane.begin();
 
 
+    planeItr->setx2y(ptr2);
+    planeItr++;
+    planeItr->sety2z(ptr3);
+    planeItr->setx2z(ptr1);
+    planeItr++;
+    planeItr->sety2z(ptr2);
 
     planeItr = plane.begin();
     checkVal = checker(planeItr);
+    while (planeItr != plane.end())
+    {
 
-        while (checkVal == true)
+
+        while (checkVal != false)
        {
-        tempPoint1 = planeItr->getX();
-        tempPoint2 = planeItr->getY();
-        tempPoint3 = planeItr->getZ();
-        tempPoint4 = planeItr->getC();
 
-        ///tempVal = tempPoint4.getX()+numberGen(planeItr);
-        ///tempPoint4.setX(tempVal);
+        flipa(coin);
+        switch (coinVal)
+        {
+        case 1:
+        {
+            tempPoint1 = planeItr->getX();
+            tempPoint2 = planeItr->getY();
+            tempPoint3 = planeItr->getZ();
+            tempPoint4 = planeItr->getC();
+            plane.push_back(surface(tempPoint1, tempPoint2, tempPoint4));
+            ptr3 = plane.end();
+            ptr3--;
+            plane.push_back(surface(tempPoint4, tempPoint2, tempPoint3));
+            ptr2 = plane.end();
+            ptr2--;
+            plane.push_back(surface(tempPoint1, tempPoint4, tempPoint3));
+            ptr1  = plane.end();
+            ptr1--;
 
-        ///tempVal = (tempPoint4.getY()+numberGen(planeItr));
-        ///tempPoint4.setY(tempVal);
-        ///tempVal = (tempPoint4.getZ()+numberGen(planeItr));
-        ///tempPoint4.setZ(tempVal);
-        plane.push_back(surface(tempPoint1, tempPoint2, tempPoint4));
-        plane.push_back(surface(tempPoint4, tempPoint2, tempPoint3));
-        plane.push_back(surface(tempPoint1, tempPoint4, tempPoint3));
+            ptr3->setx2y(planeItr->getx2y());
+            ptr3->setx2z(ptr1);
+            ptr3->sety2z(ptr2);
 
-        plane.pop_front();
+            ptr2->sety2z(planeItr->gety2z());
+            ptr2->setx2y(ptr3);
+            ptr2->setx2z(ptr1);
+
+            ptr1->setx2z(planeItr->getx2z());
+            ptr1->setx2y(ptr3);
+            ptr1->sety2z(ptr2);
+
+            planeDelete = planeItr;
+            planeItr++;
+            planeDelete = plane.erase(planeDelete);
+            checkVal = checker(planeItr);
+        }
+        default:
+        {
+            d3val = tossa(coin);
+            switch (d3val)
+            {
+            case 1:
+                {
+                    if (planeItr->getx2zFlag() == false)
+                        d3val++;
+                    else
+                    {
+                        ptr1 = planeItr->getx2z();
+                        tempPoint1 = ptr1->getX();
+                        tempPoint2 = ptr1->getZ();
+                        tempPoint3 = ptr1->getY();
+                        tempPoint4 = planeItr->getY();
+                        tempVal = ((tempPoint2.getX()+tempPoint3.getX())/2);
+                        tempPoint5.setX(tempVal);
+                        tempVal = ((tempPoint2.getY()+tempPoint3.getY())/2);
+                        tempPoint5.setY(tempVal);
+                        tempVal = ((tempPoint2.getZ()+tempPoint3.getZ())/2);
+                        tempPoint5.setZ(tempVal);
+                        tempPoint5 = centroid(tempPoint2,tempPoint3);
+                        plane.push_back(surface(tempPoint1,tempPoint3,tempPoint5));
+                        plane.push_back(surface(tempPoint1,tempPoint5,tempPoint2));
+                        plane.push_back(surface(tempPoint3,tempPoint4,tempPoint5));
+                        plane.push_back(surface(tempPoint5,tempPoint4,tempPoint2));
+                        ptr1 = plane.erase(ptr1);
+                        planeDelete = planeItr;
+                        planeItr++;
+                        planeDelete = plane.erase(planeDelete);
+                        checkVal = checker(planeItr);
+
+                    }
+                    break;
+
+                }
+            case 2:
+                {
+                    if (planeItr->getx2yFlag() == false)
+                        d3val++;
+                    else
+                    {
+
+                    break;
+                    }
+                }
+            case 3:
+                {
+                    if (planeItr->gety2zFlag() == false)
+                        break;
+                    else
+                    {
+
+                    }
+                    break;
+                }
+            }
+
+        }
+        }
         checkVal = checker(planeItr);
+    }
+    planeItr++;
     }
 
     planeItr = plane.begin();
+
 
     while(planeItr != plane.end())
     {
@@ -80,7 +188,7 @@ int main()
         outdat << trunc(tempPoint1.getX()) << " " << trunc(tempPoint1.getY()) << " " << trunc(tempPoint1.getZ()) << endl;
         outdat << trunc(tempPoint2.getX()) << " " << trunc(tempPoint2.getY()) << " " << trunc(tempPoint2.getZ()) << endl;
         outdat << trunc(tempPoint3.getX()) << " " << trunc(tempPoint3.getY()) << " " << trunc(tempPoint3.getZ()) << endl;
-        outdat << trunc(tempPoint1.getX()) << " " << trunc(tempPoint1.getY()) << " " << trunc(tempPoint1.getZ()) << endl;
+        outdat << trunc(tempPoint1.getX()) << " " << trunc(tempPoint1.getY()) << " " << trunc(tempPoint1.getZ()) << endl << endl;;
 
         planeItr++;
     }
@@ -88,13 +196,10 @@ int main()
 
     outfile << "clear" << endl ;
     outfile << "reset" << endl ;
-    outfile << "set hidden" << endl ;
-    ///outfile << "set pm3d" << endl ;
-    ///outfile << "set surface" << endl ;
-    //outfile << "set dgrid3d" << endl;
-    outfile << "set xrange [0:200]" << endl;
-    outfile << "set yrange [0:200]" << endl;
-    outfile << "set zrange [0:200]" << endl;
+   /// outfile << "set hidden3d" << endl;
+    outfile << "set xrange [-1000:2000]" << endl;
+    outfile << "set yrange [-1000:2000]" << endl;
+    outfile << "set zrange [-1000:2000]" << endl;
     outfile << "splot \"data.txt\" with lines" << endl;
     outfile << "pause -1";
 
@@ -103,7 +208,7 @@ int main()
     outdat.close();
 
     system("gnuplot command.txt ");
-
+    plane.clear();
     return 0;
 }
 
@@ -113,7 +218,7 @@ int main()
 
 bool checker(list<surface>::iterator itr)
 {
-    double xVal,yVal,zVal, val, maxVal =35;
+    double xVal,yVal,zVal, val, maxVal =500;
     point X,Y,Z,C;
 
     X= itr->getX();
@@ -121,9 +226,9 @@ bool checker(list<surface>::iterator itr)
     Z= itr->getZ();
     C= itr->getC();
 
-    xVal = abs(trunc(sqrt(((pow(X.getX() - C.getX(), 2))-(pow(X.getY() - C.getY(), 2))-(pow(X.getZ() - C.getZ(), 2))))));
-    yVal = abs(trunc(sqrt(((pow(Y.getX() - C.getX(), 2))-(pow(Y.getY() - C.getY(), 2))-(pow(Y.getZ() - C.getZ(), 2))))));
-    zVal = abs(trunc(sqrt(((pow(Z.getX() - C.getX(), 2))-(pow(Z.getY() - C.getY(), 2))-(pow(Z.getZ() - C.getZ(), 2))))));
+    xVal = abs(trunc(sqrt(((pow(X.getX() - C.getX(), 2))+(pow(X.getY() - C.getY(), 2))+(pow(X.getZ() - C.getZ(), 2))))));
+    yVal = abs(trunc(sqrt(((pow(Y.getX() - C.getX(), 2))+(pow(Y.getY() - C.getY(), 2))+(pow(Y.getZ() - C.getZ(), 2))))));
+    zVal = abs(trunc(sqrt(((pow(Z.getX() - C.getX(), 2))+(pow(Z.getY() - C.getY(), 2))+(pow(Z.getZ() - C.getZ(), 2))))));
 
     if ((xVal <= maxVal) || (yVal <= maxVal) || (zVal <= maxVal))
         return false;
@@ -131,24 +236,3 @@ bool checker(list<surface>::iterator itr)
         return true;
 }
 
-
-double numberGen(list<surface>::iterator itr)
-{
-        double xVal,yVal,zVal, val, shiftVal;
-        point X,Y,Z,C;
-
-    X= itr->getX();
-    Y= itr->getY();
-    Z= itr->getZ();
-    C= itr->getC();
-
-    xVal = abs(trunc(sqrt(((pow(X.getX() - C.getX(), 2))-(pow(X.getY() - C.getY(), 2))-(pow(X.getZ() - C.getZ(), 2))))));
-    yVal = abs(trunc(sqrt(((pow(Y.getX() - C.getX(), 2))-(pow(Y.getY() - C.getY(), 2))-(pow(Y.getZ() - C.getZ(), 2))))));
-    zVal = abs(trunc(sqrt(((pow(Z.getX() - C.getX(), 2))-(pow(Z.getY() - C.getY(), 2))-(pow(Z.getZ() - C.getZ(), 2))))));
-
-    shiftVal = trunc(abs(xVal+yVal+zVal)/2);
-    default_random_engine gen;
-    uniform_real_distribution<double> rando ((-shiftVal),(shiftVal));
-
-    return rando(gen);
-}
--- classDefs.h
+++ classDefs.h
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <random>
+#include <list>
 #include <iterator>
 using namespace std;
 
@@ -51,23 +52,41 @@ void displayPoint(point x);
 class surface
 {
 public:
+    surface()
+    {
+        x2yFlag = false;
+        x2zFlag = false;
+        y2zFlag = false;
+    }
     surface(point x, point y, point z)
         {
             xPoint = x;
             yPoint = y;
             zPoint = z;
             cent = centroid(xPoint,yPoint,zPoint);
+            x2yFlag = false;
+            x2zFlag = false;
+            y2zFlag = false;
         }
 
     void setX(point x){xPoint = x;}
     void setY(point x){yPoint = x;}
     void setZ(point x){zPoint = x;}
     void setC(point x){cent = x;}
+    void setx2y(list<surface>::iterator x){x2y = x; x2yFlag = true;}
+    void setx2z(list<surface>::iterator x){x2z = x; x2zFlag = true;}
+    void sety2z(list<surface>::iterator x){y2z = x; y2zFlag = true;}
 
     point getX(){return xPoint;}
     point getY(){return yPoint;}
     point getZ(){return zPoint;}
     point getC(){return cent;}
+    list<surface>::iterator getx2y(){return x2y;}
+    list<surface>::iterator getx2z(){return x2z;}
+    list<surface>::iterator gety2z(){return y2z;}
+    bool getx2yFlag(){return x2yFlag;}
+    bool getx2zFlag(){return x2zFlag;}
+    bool gety2zFlag(){return y2zFlag;}
 
     void displayPoints()
     {
@@ -84,6 +103,8 @@ public:
 
 private:
     point xPoint, yPoint, zPoint, cent;
+    list<surface>::iterator x2y, x2z, y2z;
+    bool x2yFlag, x2zFlag, y2zFlag;
 };
 
 
@@ -94,16 +115,50 @@ private:
 
 point centroid(point x, point y, point z)
     {
-        double val;
+        double xval, yval, zval, shiftVal, tempVal;
         point center;
 
-        center.setX(trunc((x.getX()+y.getX()+z.getX())/3));
-        center.setY(trunc((x.getY()+y.getY()+z.getY())/3));
-        center.setZ(trunc((x.getZ()+y.getZ()+z.getZ())/3));
 
+        xval = abs(trunc((x.getX()+y.getX()+z.getX())/3));
+        yval = abs(trunc((x.getY()+y.getY()+z.getY())/3));
+        zval = abs(trunc((x.getZ()+y.getZ()+z.getZ())/3));
+
+        shiftVal = trunc((xval + yval + zval)/3);
+        default_random_engine gen(time(NULL));
+        uniform_real_distribution<double> rando (-(shiftVal/3),((shiftVal/2)));
+        tempVal = (rando(gen) + xval);
+        center.setX(tempVal);
+        tempVal = (rando(gen) + yval);
+        center.setY(tempVal);
+        tempVal = (rando(gen) + zval);
+        center.setZ(tempVal);
         return center;
     }
 void displayPoint(point x)
     {
         cout << "\nX val: " << trunc(x.getX()) << "\nY val: " << trunc(x.getY()) << "\nZ val: " << trunc(x.getZ()) << endl;
+    }
+
+///=======================================================================
+
+point centroid(point x, point y)
+    {
+        double xval, yval, zval, shiftVal, tempVal;
+        point center;
+
+
+        xval = trunc((x.getX()+y.getX())/2);
+        yval = trunc((x.getY()+y.getY())/2);
+        zval = trunc((x.getZ()+y.getZ())/2);
+
+        shiftVal = trunc((xval + yval + zval)/3);
+        default_random_engine gen(time(NULL));
+        uniform_real_distribution<double> rando (-(shiftVal/2),(shiftVal/2));
+        tempVal = (rando(gen) + xval);
+        center.setX(tempVal);
+        tempVal = (rando(gen) + yval);
+        center.setY(tempVal);
+        tempVal = (rando(gen) + zval);
+        center.setZ(tempVal);
+        return center;
     }
